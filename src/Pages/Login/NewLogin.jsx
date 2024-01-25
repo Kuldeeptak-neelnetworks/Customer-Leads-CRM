3630edcce5c6773f6e04edfb4d802d5aa53fc2c7
Deven Umrania <deven@neelnetworks.com>
2024-01-25 19:02:13 +0530

    update: modified login page

--- src/Pages/Login/NewLogin.jsx
+++ src/Pages/Login/NewLogin.jsx
@@ -1,23 +1,31 @@
+import { Link } from "react-router-dom";
+
 import logo from "../../assets/logo.png";
+import avatar from "../../assets/avatar.png";
 
 const NewLogin = () => {
   return (
     <div className="bg-wrapper">
       <div className="content-parent">
-        <div className=""></div>
-        <form>
-          <div>
+        <div className="text-center">
+          <img src={avatar} alt="avatar" />
+          <h2 className="login-heading">Welcome</h2>
+        </div>
+        <form className="login-form">
+          <div className="group mb-4">
             <label>Username</label>
-            <input />
+            <input type="email" />
           </div>
-          <div>
+          <div className="group mb-2">
             <label>Password</label>
-            <input />
+            <input type="password" />
           </div>
-          <p>Forgot Password?</p>
-          <button>LOGIN</button>
+          <div className="text-end mb-2">
+            <Link className="link">Forgot Password?</Link>
+          </div>
+          <button className="w-100 login-btn">LOGIN</button>
         </form>
-        <img src={logo} alt="logo" />
+        <img className="pt-5 w-100" src={logo} alt="logo" />
       </div>
     </div>
   );
