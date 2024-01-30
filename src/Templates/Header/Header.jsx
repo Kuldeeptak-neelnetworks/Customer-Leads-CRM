3d497dea980480d6b418d73f21fc3729c8868d1d
Deven Umrania <deven@neelnetworks.com>
2024-01-30 11:01:13 +0530

    fix: minor fixes in header & sidebar

--- src/Templates/Header/Header.jsx
+++ src/Templates/Header/Header.jsx
@@ -2,10 +2,15 @@ import { NotificationIconSVG } from "../../utils/SVGs/SVGs";
 import HeaderPopover from "./HeaderPopover";
 
 const Header = () => {
+  const username = JSON.parse(localStorage.getItem("user"))?.name;
+
+  const capitalUserName = (username) =>
+    username.slice(0, 1).toUpperCase().concat(username.slice(1)) ?? "";
+
   return (
     <div className="header w-100 d-flex justify-content-end align-items-center">
       <div className="d-flex align-items-center gap-3">
-        <p className="m-0 username">Deven</p>
+        <p className="m-0 username">{capitalUserName(username)}</p>
         <div className="header-icon">
           <HeaderPopover />
         </div>
